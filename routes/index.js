f3c2845787ff07753b60a59c71657e3a30b856ad
Abhimanyu310 <abhip310@gmail.com>
2016-10-01 22:00:46 -0600

    Created basic model, made connection, and output on html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
+var models  = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get('/projects', function(req, res, next) {
+    models.Project.findAll().then(function(projects) {
+        res.render('project_list', {
+            title: 'Sequelize: Express Example',
+            projects: projects
+        });
+    });
 });
 
 module.exports = router;
