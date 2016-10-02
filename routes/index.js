5be1255e2438af155b8c25843c356d30b30b8402
Abhimanyu310 <abhip310@gmail.com>
2016-10-02 01:30:49 -0600

    Can take input from from and save to DB

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var models  = require('../models');
+var Project = require('../models/project');
 var express = require('express');
 var router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +11,27 @@ router.get('/', function(req, res, next) {
 });
 
 
+// Project submission for faculty
+router.get('/submit-project', function(req, res, next) {
+    res.render('Faculty_Form', { title: "Hah" });
+});
+
+// Post the project submission form
+router.post('/submit-project', function(req, res, next) {
+    models.Project.create({name: req.body.name}).then(function (task) {
+        res.render('Faculty_Form', { title: task.name });
+    }).catch(function (error) {
+        //error handling
+    });
+    // var project = models.Project.build({name: req.body.name});
+    // project.save().then(function() {
+    //     res.render('Faculty_Form', { title: "SAVED!"});
+    // })
+    // res.render('Faculty_Form', { title: req.body.name });
+});
+
+
+// GET list of projects page
 router.get('/projects', function(req, res, next) {
     models.Project.findAll().then(function(projects) {
         res.render('project_list', {
