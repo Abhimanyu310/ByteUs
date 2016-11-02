8f22ac69279588ec06a9a53bd994437a83c542c4
Abhimanyu310 <abhip310@gmail.com>
2016-11-02 01:31:22 -0600

    Started on info box and validation

--- controllers/ProjectController.js
+++ controllers/ProjectController.js
@@ -26,84 +26,100 @@ module.exports = {
     },
     
     getCreateProject: function(req, res, next) {
-        var messages = req.flash('error');
-        console.log(messages);
+        var errors = req.flash('errors');
+        var success = req.flash('success');
+        var form_data = req.flash('form_data');
+        // console.log(messages);
         res.render('project/form', {
-            title: 'Faculty Form', messages: messages, csrfToken: req.csrfToken()
+            title: 'Faculty Form',
+            errors: errors,
+            hasErrors: errors.length > 0,
+            success: success,
+            hasSuccess: success.length > 0,
+            formData: form_data[0],
+            csrfToken: req.csrfToken()
         });
     },
     
     postCreateProject: function(req, res, next) {
         req.checkBody('email', 'Invalid email').notEmpty().isEmail();
         req.checkBody('name', 'Invalid name').notEmpty();
+        req.checkBody('phone', 'Invalid phone').notEmpty().isInt();
         var errors = req.validationErrors();
         if (errors) {
             var messages = [];
             errors.forEach(function (error) {
                 messages.push(error.msg);
             });
-            req.flash('error', messages);
+            console.log(errors)
+            req.flash('errors', messages);
+            req.flash('form_data', req.body);
+            // req.flash('success', "HAHA")
             res.redirect('/project/form');
         }
-
-        if (req.body.areas){
-            var areas = req.body.areas[0];
-            for(i = 1; i < req.body.areas.length; i++){
-                areas +=  ', ' + req.body.areas[i];
+        else{
+            if (req.body.areas){
+                var areas = req.body.areas[0];
+                for(i = 1; i < req.body.areas.length; i++){
+                    areas +=  ', ' + req.body.areas[i];
+                }
             }
+
+            var project = models.Project.create({
+                description: req.body.description,
+                url: req.body.url,
+                requirements1: req.body.requirements1,
+                requirements2: req.body.requirements2,
+                requirements3: req.body.requirements3,
+                requirements4: req.body.requirements4,
+                requirements5: req.body.requirements5,
+                longdescription: req.body.longdescription,
+                areas: areas,
+                supervision_req: req.body.supervision_req,
+                supervision_provided: req.body.supervision_provided,
+                nature_of_work: req.body.nature_of_work,
+                nature_of_work_other: req.body.nature_of_work_other,
+                prior_work: req.body.prior_work,
+                prior_work_other: req.body.prior_work_other,
+                match_of_funding: req.body.match_of_funding,
+                not_sure: req.body.not_sure,
+                contact: req.body.contact,
+                Faculty: {
+                    name: req.body.name,
+                    phone: req.body.phone,
+                    email: req.body.email,
+                    faculty_department: req.body.faculty_department,
+                    edc: req.body.edc,
+                    secondary_name: req.body.secondary_name,
+                    secondary_phone: req.body.secondary_phone,
+                    secondary_email: req.body.secondary_email,
+                    secondary_faculty_department: req.body.secondary_faculty_department,
+                    post_doc_name: req.body.post_doc_name,
+                    post_doc_phone1: req.body.post_doc_phone1,
+                    post_doc_phone2: req.body.post_doc_phone2,
+                    post_doc_phone3: req.body.post_doc_phone3,
+                    post_doc_email: req.body.post_doc_email,
+                    supervised_past: req.body.supervised_past,
+                    specific_students1: req.body.specific_students1,
+                    specific_students2: req.body.specific_students2,
+                    specific_students3: req.body.specific_students3
+                }
+            }, {
+                include: [ {model: models.FacultyInfo, as: 'Faculty'} ]
+            }).then(function (task) {
+                // task.setFaculty([faculty]).then(function () {
+                //         //done
+                //     });
+                // console.log(task)
+                req.flash('success', 'Project Submission Successful!');
+                res.redirect('success');
+            }).catch(function (error) {
+                //error handling
+                // console.log(error)
+            });
         }
 
-        var project = models.Project.create({
-            description: req.body.description,
-            url: req.body.url,
-            requirements1: req.body.requirements1,
-            requirements2: req.body.requirements2,
-            requirements3: req.body.requirements3,
-            requirements4: req.body.requirements4,
-            requirements5: req.body.requirements5,
-            longdescription: req.body.longdescription,
-            areas: areas,
-            supervision_req: req.body.supervision_req,
-            supervision_provided: req.body.supervision_provided,
-            nature_of_work: req.body.nature_of_work,
-            nature_of_work_other: req.body.nature_of_work_other,
-            prior_work: req.body.prior_work,
-            prior_work_other: req.body.prior_work_other,
-            match_of_funding: req.body.match_of_funding,
-            not_sure: req.body.not_sure,
-            contact: req.body.contact,
-            Faculty: {
-                name: req.body.name,
-                phone: req.body.phone,
-                email: req.body.email,
-                faculty_department: req.body.faculty_department,
-                edc: req.body.edc,
-                secondary_name: req.body.secondary_name,
-                secondary_phone: req.body.secondary_phone,
-                secondary_email: req.body.secondary_email,
-                secondary_faculty_department: req.body.secondary_faculty_department,
-                post_doc_name: req.body.post_doc_name,
-                post_doc_phone1: req.body.post_doc_phone1,
-                post_doc_phone2: req.body.post_doc_phone2,
-                post_doc_phone3: req.body.post_doc_phone3,
-                post_doc_email: req.body.post_doc_email,
-                supervised_past: req.body.supervised_past,
-                specific_students1: req.body.specific_students1,
-                specific_students2: req.body.specific_students2,
-                specific_students3: req.body.specific_students3
-            }
-        }, {
-            include: [ {model: models.FacultyInfo, as: 'Faculty'} ]
-        }).then(function (task) {
-            // task.setFaculty([faculty]).then(function () {
-            //         //done
-            //     });
-            // console.log(task)
-            res.redirect('success');
-        }).catch(function (error) {
-            //error handling
-            // console.log(error)
-        });
+
     },
     
     getProjectDetail: function(req, res, next) {
@@ -112,7 +128,7 @@ module.exports = {
             where: {id: projectId},
             include: [ {model: models.FacultyInfo, as: 'Faculty'} ]
         }).then(function (project){
-            console.log(project)
+            // console.log(project)
             res.render('project/project-single', {
                 title: "Project Detail",
                 project: project
@@ -123,7 +139,12 @@ module.exports = {
     },
 
     getProjectSuccess: function(req, res, next) {
-        res.render('project/success', { title: "Success" });
+        var success = req.flash('success');
+        res.render('project/success', {
+            title: "Success",
+            success: success,
+            hasSuccess: success.length > 0
+        });
     },
 
     postSearchProject: function(req, res, next) {
@@ -158,7 +179,7 @@ function postSearchProjectByTitle(req, res, next) {
             ],
         include: [ {model: models.FacultyInfo, as: 'Faculty'} ]
     }).then(function (projects){
-        console.log(projects);
+        // console.log(projects);
         res.render('project/project-list', {
             title: "Search Results",
             header: "Search Results",
@@ -185,7 +206,7 @@ function postSearchProjectByDescription(req, res, next) {
         ],
         include: [ {model: models.FacultyInfo, as: 'Faculty'} ]
     }).then(function (projects){
-        console.log(projects);
+        // console.log(projects);
         res.render('project/project-list', {
             title: "Search Results",
             header: "Search Results",
@@ -215,7 +236,7 @@ function postSearchProjectByFaculty(req, res, next) {
             }
         }]
     }).then(function (projects){
-        console.log(projects);
+        // console.log(projects);
         res.render('project/project-list', {
             title: "Search Results",
             header: "Search Results",
