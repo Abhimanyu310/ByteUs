91fb9f46a1e37be4f711e402b4417ea8fbbea4e0
Abhimanyu310 <abhip310@gmail.com>
2016-11-02 22:32:04 -0600

    Student can submit resume and cover letter

--- controllers/ApplicationController.js
+++ controllers/ApplicationController.js
@@ -12,6 +12,35 @@ module.exports = {
 
     postApplicationForm: function(req, res, next) {
         console.log('done');
+        if (!req.files) {
+            // console.log("no file");
+        }
+        if (req.files) {
+            // console.log("FILES YAY");
+            student_id = req.body.student_id;
+
+            resume = req.files.resume;
+
+            resume.mv('uploads/resume-'+student_id, function(err) {
+                if (err) {
+                    console.log('error');
+                }
+                else {
+                    console.log('File uploaded!');
+                }
+            });
+
+            cover_letter = req.files.cover_letter;
+            cover_letter.mv('uploads/cover_letter-'+student_id, function(err) {
+                if (err) {
+                    console.log('error');
+                }
+                else {
+                    console.log('File uploaded!');
+                }
+            });
+        }
+        res.redirect('/');
         // res.render('application/form', {
         //     title: "Student",
         //     csrfToken: req.csrfToken()
