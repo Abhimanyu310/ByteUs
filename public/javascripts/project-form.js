c48e56915ba680cd4c45c70f0058a0a3c83c1ff5
samwise3 <samueltobey@gmail.com>
2016-11-03 10:26:00 -0600

    Word count and length restriction. Added for project description and optional requirements.

--- public/javascripts/project-form.js
+++ public/javascripts/project-form.js
@@ -1,20 +1,38 @@
 'use strict';
 
-/* Phone Numbers */
+$(document).ready(function () {
 
-// Strip everything but 10 numbers when the field loses focus
-$('#form_primPhone, #form_secPhone, #form_postDocPhone').on('focusout', function() {
-  var $phones = $(this);
-  $phones.each(function(idx,elem) {
-    var $elem = $(elem); 
-    $elem.val(function(){
-      return $elem.val().replace(/\D/g,'').substring(0,10);
+  /* Phone Numbers */
+  
+  // Strip everything but 10 numbers when the field loses focus
+  $('#form_primPhone, #form_secPhone, #form_postDocPhone').on('focusout', function() {
+    var $phones = $(this);
+    $phones.each(function(idx,elem) {
+      var $elem = $(elem); 
+      $elem.val(function(){
+        return $elem.val().replace(/\D/g,'').substring(0,10);
+      });
     });
   });
+
+  /* Word Counts */
+  
+  // Truncate excess characters and display the cound in the sibling span element
+  $('#description, #requirements').each(function(idx,elem) {
+    var max_chars = $(elem).attr('maxlength');
+    $(elem).siblings('#char_count').html(max_chars);  
+    $(elem).on('keyup', function() {
+      var chars = elem.value.length;
+      if (chars > max_chars) {
+        var trunc = $(this).val().substring(0, max_chars);
+        $(elem).val(trunc);
+      }
+      else {
+        $(elem).siblings('#char_count').html(max_chars - chars);
+      }
+    });
+  });
+  
+  // TODO: Logic to prevent form submission if bad input
+
 });
-
-/* Word Counts */
-
-$('#word_count_desc')
-
-// TODO: Logic to prevent form submission if bad input
