0e04d76f47664a9db9394646f3b18bd82fe5984a
samwise3 <samueltobey@gmail.com>
2016-11-03 21:31:12 -0600

    Hide or show & clear added for 'other' radio selection

--- public/javascripts/project-form.js
+++ public/javascripts/project-form.js
@@ -1,8 +1,48 @@
 'use strict';
 
+// Deselect event for radio buttons.
+// From: <http://stackoverflow.com/questions/11173685/how-to-detect-radio-button-deselect-event>
+// See also: <http://jsfiddle.net/s7f9s/2/>
+
+function setupDeselectEvent() {
+  var selected = {};
+  $('input[id=nature], input[id=amount]').on('click', function() {
+    if(this.name in selected && this != selected[this.name])
+      $(selected[this.name]).trigger("deselect");
+    selected[this.name] = this;
+  }).filter(':checked').each(function() {
+    selected[this.name] = this;
+  });
+}
+
 $(document).ready(function () {
 
-  /* Phone Numbers */
+  /**
+   * Hide or show "other" text input field
+   */
+  
+  // Initially hide text input fields
+  $('#nature[name=nature_of_work_other], #amount[name=prior_work_other]')
+    .each(function(idx,elem) {
+      $(elem).hide();
+    }
+  );
+
+  // See function above
+  setupDeselectEvent(true);
+
+  // show/hide & clear
+  $('#nature[value=Other], #amount[value=Other]').each(function(idx,elem) {
+    $(elem).on('deselect', function() {
+      $(elem).siblings('input[type=text]').hide().val('');
+    }).on('change', function() {
+      $(elem).siblings('input[type=text]').show();
+    });
+  });
+
+  /**
+   * Phone numbers
+   */
   
   // Strip everything but 10 numbers when the field loses focus
   $('#form_primPhone, #form_secPhone, #form_postDocPhone').on('focusout', function() {
@@ -15,7 +55,9 @@ $(document).ready(function () {
     });
   });
 
-  /* Word Counts */
+  /**
+   * Word Counts 
+   */
   
   // Truncate excess characters and display the count in the sibling span element
   $('#description, #requirements, #longdescription').each(function(idx,elem) {
