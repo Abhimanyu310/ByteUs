3f8fb6479503f1f9b75834e658c7183bdc7c3bfd
Abhimanyu310 <abhip310@gmail.com>
2016-11-18 21:18:01 -0700

    redirect bug

--- controllers/helpers/helpers.js
+++ controllers/helpers/helpers.js
@@ -2,9 +2,12 @@ exports.isLoggedIn = function (req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
-    req.session.oldUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    // req.session.oldUrl = '/project' + req.url;
-    res.redirect('/');
+    if (req.originalUrl != '/user/logout'){
+        req.session.oldUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+        // req.session.oldUrl = '/project' + req.url;
+        res.redirect('/');
+    }
+
 };
 
 
@@ -12,18 +15,22 @@ exports.isLoggedInAsFaculty = function(req, res, next) {
     if (req.isAuthenticated() && (req.user.type == 'Faculty' || req.session.cu_user.type == 'Faculty')) {
         return next();
     }
-    req.session.oldUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    // req.session.oldUrl = '/project' + req.url;
-    res.redirect('/');
+    if (req.originalUrl != '/user/logout'){
+        req.session.oldUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+        // req.session.oldUrl = '/project' + req.url;
+        res.redirect('/');
+    }
 };
 
 exports.isLoggedInAsStudent = function(req, res, next) {
     if (req.isAuthenticated() && (req.user.type == 'Student' || req.session.cu_user.type == 'Student')) {
         return next();
     }
-    req.session.oldUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    // req.session.oldUrl = '/project' + req.url;
-    res.redirect('/');
+    if (req.originalUrl != '/user/logout'){
+        req.session.oldUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+        // req.session.oldUrl = '/project' + req.url;
+        res.redirect('/');
+    }
 };
 
 exports.notLoggedIn = function(req, res, next) {
