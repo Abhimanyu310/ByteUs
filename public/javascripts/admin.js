c643a9994e98683dd10fe378fb2a53b79430c139
Logeswari Shanmugavel <logeswarishanmugavel@Logeswaris-MacBook-Pro.local>
2016-12-07 15:22:20 -0700

    Student details in Admin page

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,6 +1,20 @@
 'use strict';
 
 $(document).ready(function() {
-
-
-}
+	var setvalues = function(event) {
+		console.log('inside fn');	
+    	var value = this.value;
+	
+		console.log(value);
+		$('#Overwrite').prop('checked',true);
+		$.ajax({
+          method: 'GET',
+          url: '/application/' + value + '/view/'
+        })
+        .done(function(obj) {
+        	var application = obj.application;
+        	console.log(application);
+       	});
+    }
+    $('#AllottedStudentName').change(setvalues)
+});
