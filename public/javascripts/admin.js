836218e8b44a328950cb39d76823a6ca2f9b12d9
Logeswari Shanmugavel <logeswarishanmugavel@Logeswaris-MacBook-Pro.local>
2016-12-08 02:59:10 -0700

    Allotted Student Details in matrix display

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -23,37 +23,104 @@ $(document).ready(function() {
             //console.log(projects[i].id);
             var projectid = projects[i].id;
             console.log(projectid);
-            $('#projectname'+projects[i].id).text(projects[i].description);
-            $.ajax({
+            $('#projectname'+projectid).text(projects[i].description);
+            findmatching(projectid,projects[i].description);
+          }
+       	});
+      function findmatching(projectid,text)
+      {
+        $.ajax({
               method: 'GET',
-              url: '/admin/' + projects[i].id + '/view/'
+              url: '/admin/' + projectid + '/view/'
             })
             .done(function(matchobj) {
-            //console.log(obj);
-        	   var matched_id = matchobj.matched_id;
+              //console.log(obj);
+             var matched_id = matchobj.matched_id;
              //console.log(projects[i].id);
-        	   console.log(matched_id);
+             console.log(matched_id);
              if(matched_id!=0){
-               // console.log('True');
+                console.log('True');
                 $.ajax({
                   method: 'GET',
                   url: '/application/' + matched_id + '/view/'
                 })
                 .done(function(studentobj) {
-                  //console.log(studentobj);
+                  console.log(studentobj);
+                  console.log(text);
                   student = studentobj.application;
-                  //console.log(student.name);
+                  $('#AllottedStudentname'+projectid).text(student.name);
+                  $('#sid'+projectid).text(student.sid);
+                  $('#primajor'+projectid).text(student.Academics.primary_major);
+                  $('#secmajor'+projectid).text(student.Academics.secondary_major);
+                  $('#levelinschool'+projectid).text(student.Academics.next_fall_level);
+                  $('#graddate'+projectid).text(student.Academics.grad_month+'/'+student.Academics.grad_year);
+                  $('#gpa'+projectid).text(student.Academics.gpa);
+                  $('#gender'+projectid).text(student.gender);
+                  $('#ethnicity'+projectid).text(student.race);
+                  $('#previousexperience'+projectid).text(student.prev_research_exp);
+                  $('#otheremployment'+projectid).text(student.Apprenticeship.prev_application);
+                  if(student.Apprenticeship.most_interest)
+                  {  
+                    $('#mostint'+projectid).attr("href",'/project/'+student.Apprenticeship.most_interest+'/view');
+                    $('#mostint'+projectid).text("Most Int");
+                  }
+                  else
+                  {
+                    $('#mostint'+projectid).hide();
+
+                  }
+                  if(student.Apprenticeship.high_interest)
+                  {  
+                    $('#highint'+projectid).attr("href",'/project/'+student.Apprenticeship.high_interest+'/view');
+                    $('#highint'+projectid).text("High Int");
+                  }
+                  else
+                  {
+                    $('#highint'+projectid).hide();
+
+                  }
+                  if(student.Apprenticeship.moderate_interest)
+                  {  
+                    $('#modint'+projectid).attr("href",'/project/'+student.Apprenticeship.moderate_interest+'/view');
+                    $('#modint'+projectid).text("Mode Int");
+                  }
+                  else
+                  {
+                    $('#modint'+projectid).hide();
+
+                  }
+                  if(student.Apprenticeship.low_interest)
+                  {  
+                    $('#lowint'+projectid).attr("href",'/project/'+student.Apprenticeship.low_interest+'/view');
+                    $('#lowint'+projectid).text("low Int");
+                  }
+                  else
+                  {
+                    $('#lowint'+projectid).hide();
+
+                  }
+                  if(student.Apprenticeship.least_interest)
+                  {  
+                    $('#leastint'+projectid).attr("href",'/project/'+student.Apprenticeship.least_interest+'/view');
+                    $('#leastint'+projectid).text("least Int");
+                  }
+                  else
+                  {
+                    $('#leastint'+projectid).hide();
+
+                  }
                 });
              }
+             else
+             {
+              $('#mostint'+projectid).hide();
+              $('#highint'+projectid).hide();
+              $('#modint'+projectid).hide();
+              $('#lowint'+projectid).hide();
+              $('#leastint'+projectid).hide();
+             }
             });
-            console.log(i);
-            //console.log(student);
-            if(student){
-              $('#AllottedStudentname'+i).text(student.name);
-              console.log(student.name);
-            }
-          }
-       	});
+      }
   //    }
   //$('.match').on('click', showvalues);
 });
